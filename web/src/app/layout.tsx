e4e3927f03cb61cdef82230a18cbf4be2b5c90bf
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-05-18 00:01:48 -0300

    feat: HomePage

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,18 +1,33 @@
 import { ReactNode } from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
 
 export const metadata = {
-  title: 'SpaceTime',
-  description: 'SpaceTime',
+  title: 'NLW SpaceTime',
+  description:
+    'Uma capsula do tempo construida com React, Next.js, TailWindCSS e Typescript',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
